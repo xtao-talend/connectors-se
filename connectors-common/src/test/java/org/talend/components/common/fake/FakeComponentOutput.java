450028bffb85a7a0586bfc91e111bcbee0471b6a
Dmytro Sylaiev <dmytro.sylaiev@globallogic.com>
2019-09-24 13:58:01 +0300

    fix(TDI-42576): Move fake test to the common module

--- connectors-common/src/test/java/org/talend/components/common/fake/FakeComponentOutput.java
+++ connectors-common/src/test/java/org/talend/components/common/fake/FakeComponentOutput.java
@@ -10,8 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.talend.components.azure.fake;
-
+package org.talend.components.common.fake;
 import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
