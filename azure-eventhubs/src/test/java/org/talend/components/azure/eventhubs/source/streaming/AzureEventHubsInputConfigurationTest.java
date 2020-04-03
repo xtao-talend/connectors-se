eee52f02a6cea731b3442684e96a4d35198b66de
jzhao <jzhao@talend.com>
2020-04-03 16:26:41 +0800

    fix(TDI-42242):failed test.

--- azure-eventhubs/src/test/java/org/talend/components/azure/eventhubs/source/streaming/AzureEventHubsInputConfigurationTest.java
+++ azure-eventhubs/src/test/java/org/talend/components/azure/eventhubs/source/streaming/AzureEventHubsInputConfigurationTest.java
@@ -13,43 +13,58 @@
 package org.talend.components.azure.eventhubs.source.streaming;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.List;
+import java.io.Serializable;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.talend.components.azure.eventhubs.AzureEventHubsRWTestBase;
+import org.talend.components.azure.common.Protocol;
+import org.talend.components.azure.common.connection.AzureStorageConnectionAccount;
 import org.talend.components.azure.eventhubs.dataset.AzureEventHubsDataSet;
+import org.talend.components.azure.eventhubs.datastore.AzureEventHubsDataStore;
 import org.talend.sdk.component.api.record.Record;
+import org.talend.sdk.component.junit.BaseComponentsHandler;
+import org.talend.sdk.component.junit5.Injected;
 import org.talend.sdk.component.junit5.WithComponents;
 import org.talend.sdk.component.runtime.input.Mapper;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Data
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @WithComponents("org.talend.components.azure.eventhubs")
-class AzureEventHubsInputConfigurationTest extends AzureEventHubsRWTestBase {
+class AzureEventHubsInputConfigurationTest implements Serializable {
 
     protected static final String CONSUMER_GROUP = "fake-consumer";
 
     protected static final String CONTAINER_NAME = "fake-container";
 
+    private static final String ACCOUNT_NAME = "fake-account";
+
+    private static final String ACCOUNT_KEY = "fake-key";
+
+    protected static final String EVENTHUB_NAME = "fake-eventhub";
+
+    private static final String SAS_KEY = "fake-key";
+
+    private static final String SAS_KEY_NAME = "fake-key-name";
+
+    @Injected
+    private BaseComponentsHandler componentsHandler;
+
     @Test
     @DisplayName("Test missing checkpoint connection")
     void testMissingCheckpointConnection() {
-        AzureEventHubsStreamInputConfiguration inputConfiguration = createInputConfiguration(false);
-        inputConfiguration.setDataset(createDataSet());
+        AzureEventHubsStreamInputConfiguration inputConfiguration = createInputConfiguration();
 
         inputConfiguration.setCheckpointStore(null);
         inputConfiguration.setConsumerGroupName(CONSUMER_GROUP);
         inputConfiguration.setContainerName(CONTAINER_NAME);
 
         final Mapper mapper = getComponentsHandler().createMapper(AzureEventHubsStreamInputMapper.class, inputConfiguration);
-        assertTrue(mapper.isStream());
-        getComponentsHandler().start();
         assertThrows(IllegalArgumentException.class, () -> {
             getComponentsHandler().collectAsList(Record.class, mapper, 1);
         });
@@ -59,15 +74,12 @@ class AzureEventHubsInputConfigurationTest extends AzureEventHubsRWTestBase {
     @Test
     @DisplayName("Test missing container name")
     void testMissingContainerName() {
-        AzureEventHubsStreamInputConfiguration inputConfiguration = createInputConfiguration(false);
-        inputConfiguration.setDataset(createDataSet());
+        AzureEventHubsStreamInputConfiguration inputConfiguration = createInputConfiguration();
 
         inputConfiguration.setConsumerGroupName(CONSUMER_GROUP);
         inputConfiguration.setContainerName(null);
 
         final Mapper mapper = getComponentsHandler().createMapper(AzureEventHubsStreamInputMapper.class, inputConfiguration);
-        assertTrue(mapper.isStream());
-        getComponentsHandler().start();
         assertThrows(IllegalArgumentException.class, () -> {
             getComponentsHandler().collectAsList(Record.class, mapper, 1);
         });
@@ -77,15 +89,12 @@ class AzureEventHubsInputConfigurationTest extends AzureEventHubsRWTestBase {
     @Test
     @DisplayName("Test missing consumer group")
     void testMissingConsumerGroup() {
-        AzureEventHubsStreamInputConfiguration inputConfiguration = createInputConfiguration(false);
-        inputConfiguration.setDataset(createDataSet());
+        AzureEventHubsStreamInputConfiguration inputConfiguration = createInputConfiguration();
 
         inputConfiguration.setConsumerGroupName("");
         inputConfiguration.setContainerName(CONTAINER_NAME);
 
         final Mapper mapper = getComponentsHandler().createMapper(AzureEventHubsStreamInputMapper.class, inputConfiguration);
-        assertTrue(mapper.isStream());
-        getComponentsHandler().start();
         assertThrows(IllegalArgumentException.class, () -> {
             getComponentsHandler().collectAsList(Record.class, mapper, 1);
         });
@@ -95,8 +104,7 @@ class AzureEventHubsInputConfigurationTest extends AzureEventHubsRWTestBase {
     @Test
     @DisplayName("Test missing enqueued datetime")
     void testMissingEnqueuedDateTime() {
-        AzureEventHubsStreamInputConfiguration inputConfiguration = createInputConfiguration(false);
-        inputConfiguration.setDataset(createDataSet());
+        AzureEventHubsStreamInputConfiguration inputConfiguration = createInputConfiguration();
 
         inputConfiguration.setConsumerGroupName(CONSUMER_GROUP);
         inputConfiguration.setContainerName(CONTAINER_NAME);
@@ -104,26 +112,31 @@ class AzureEventHubsInputConfigurationTest extends AzureEventHubsRWTestBase {
         inputConfiguration.setEnqueuedDateTime(null);
 
         final Mapper mapper = getComponentsHandler().createMapper(AzureEventHubsStreamInputMapper.class, inputConfiguration);
-        assertTrue(mapper.isStream());
-        getComponentsHandler().start();
         assertThrows(IllegalArgumentException.class, () -> {
             getComponentsHandler().collectAsList(Record.class, mapper, 1);
         });
 
     }
 
-    @Override
-    protected AzureEventHubsDataSet createDataSet() {
+    protected AzureEventHubsStreamInputConfiguration createInputConfiguration() {
         AzureEventHubsStreamInputConfiguration inputConfiguration = new AzureEventHubsStreamInputConfiguration();
-        final AzureEventHubsDataSet dataSet = new AzureEventHubsDataSet();
-        dataSet.setConnection(getDataStore());
-        dataSet.setValueFormat(AzureEventHubsDataSet.ValueFormat.CSV);
-        dataSet.setEventHubName(CONSUMER_GROUP);
-        return dataSet;
+        AzureEventHubsDataSet dataSet = new AzureEventHubsDataSet();
+        AzureEventHubsDataStore dataStore = new AzureEventHubsDataStore();
+        dataStore.setAuthMethod(AzureEventHubsDataStore.AuthMethod.SAS);
+        dataStore.setSasKey(SAS_KEY);
+        dataStore.setSasKeyName(SAS_KEY_NAME);
+        dataSet.setConnection(dataStore);
+        dataSet.setEventHubName(EVENTHUB_NAME);
+        inputConfiguration.setDataset(dataSet);
+
+        CheckpointStoreConfiguration storageAccount = new CheckpointStoreConfiguration();
+        storageAccount.setUseAzureSharedSignature(false);
+        AzureStorageConnectionAccount accountConn = new AzureStorageConnectionAccount();
+        accountConn.setAccountName(ACCOUNT_NAME);
+        accountConn.setAccountKey(ACCOUNT_KEY);
+        accountConn.setProtocol(Protocol.HTTPS);
+        storageAccount.setAccountConnection(accountConn);
+        return inputConfiguration;
     }
 
-    @Override
-    protected List<Record> generateSampleData(int index) {
-        throw new IllegalStateException("Not implemented!");
-    }
 }
